12c0ef49090cc5989c178148c5fcc2221236911e
Ankalakoti <C082043@exelonds.com>
2021-06-28 15:54:29 +0530

    First commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,160 @@
+class Form {
+  constructor() {
+    this.addressForm = document.querySelector('.form');
+    this.addressInput = document.querySelector('.form__input');
+    this.assets = document.querySelector('.asset-list');
+    this.error = document.querySelector('.error');
+	
+  }
+
+  _XHRRequest(url, callback) {
+    const xhr = new XMLHttpRequest();
+    try{
+    xhr.open('GET', url, true);
+    xhr.send();
+	}
+	catch(e){
+          alert("Callback Exception caught!");
+      }
+    console.log(xhr.status);
+    xhr.onreadystatechange = (e) => {
+		console.log(xhr.readyState);
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        if(document.querySelector('.spinner')) {
+           const spinner = document.querySelector('.spinner');
+		   
+
+
+        }
+        callback(xhr.responseText);
+      } else if(xhr.status == 500) {
+		  this.error.style.display = 'block';
+      }
+    }
+  }
+
+  
+  _commafy(num) {
+    var str = num.toString().split('.');
+    if (str[0].length >= 5) {
+        str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,');
+    }
+    if (str[1] && str[1].length >= 5) {
+        str[1] = str[1].replace(/(\d{3})/g, '$1 ');
+    }
+    return str.join('.');
+  }
+
+  
+
+  onSubmit() {
+    this.addressForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      // Remove old assets
+      const oldAssets = document.querySelectorAll('.asset');
+      oldAssets.forEach(asset => {
+        asset.parentNode.removeChild(asset);
+      });
+
+      // Remove old error message
+      this.error.style.display = 'none';
+    
+      const address = this.addressInput.value;
+      
+      //const addressUrl = `https://api.compound.finance/api/v2/account?addresses[]=${address}`;
+	  const tokenUrl = `http://localhost/wallet-balance/ceth/?address=${address}`;
+		const spinner = document.createElement('div');
+        spinner.className = 'spinner';
+        this.assets.append(spinner);
+      console.log('URL: ', tokenUrl);
+      this._XHRRequest(tokenUrl, (response) => {
+        
+
+        const data = JSON.parse(response);
+        console.log('Data: ', data);
+
+        if(data.Balances.length < 1) {
+          spinner.parentNode.removeChild(spinner);
+          this.error.style.display = 'block';
+          return;
+        }
+		else{
+			spinner.parentNode.removeChild(spinner);
+		}
+        
+        
+        for(let i = 0; i < data.Balances.length; i++) {
+          //const tokenUrl = `https://api.compound.finance/api/v2/ctoken?addresses[]=${tokenArray[i].address}`;
+
+            const tokenData = data.Balances[i];
+            
+            console.log('Token Data: ', tokenData);
+
+            // Create asset box
+            const asset = document.createElement('div');
+            asset.className = 'asset';
+            this.assets.appendChild(asset);
+
+            // Create asset header
+            const header = document.createElement('div');
+            header.className = 'asset__header';
+            asset.appendChild(header);
+
+			// Add icon
+            const icon = document.createElement('img');
+            icon.className = 'asset__icon'
+            icon.src = `../images/${tokenData.cTokenName.slice(1)}.svg`;
+            header.appendChild(icon);
+           
+            // Add name
+            const name = document.createElement('h4');
+            name.className = 'asset__name';
+            name.innerHTML = tokenData.cTokenName;
+            header.appendChild(name);
+
+           
+            // Create asset supply value container
+            const assetSupplyBorrowValues = document.createElement('div');
+            assetSupplyBorrowValues.className = 'asset__values asset__values--supply asset__values--active';
+            asset.appendChild(assetSupplyBorrowValues);
+
+            // Add interest rate
+            const supplyInterestRate = document.createElement('p');
+            supplyInterestRate.className = 'asset__value';
+            supplyInterestRate.innerHTML = `<strong>Supply APY Rate:</strong> ${tokenData.SupplyAPY}% APR`;
+            assetSupplyBorrowValues.appendChild(supplyInterestRate);
+
+            // Add balance
+            const borrowInterestRate = document.createElement('p');
+            borrowInterestRate.className = 'asset__value';
+            borrowInterestRate.innerHTML = `<strong>Borrow APY Rate:</strong> ${tokenData.BorrowAPY}% APR`;
+            assetSupplyBorrowValues.appendChild(borrowInterestRate);
+
+            // Add earned interest
+            const tokenBalance = document.createElement('p');
+            tokenBalance.className = 'asset__value';
+            tokenBalance.innerHTML = `<strong>Balance:</strong> ${tokenData.Balance}`;
+            assetSupplyBorrowValues.appendChild(tokenBalance);
+
+            // Add yearly interest
+            const redeemRate = document.createElement('p');
+            redeemRate.className = 'asset__value';
+            redeemRate.innerHTML = `<strong>Redeem Rate:</strong> ${tokenData.RedeemRate}`;
+            assetSupplyBorrowValues.appendChild(redeemRate);
+
+			// Add yearly interest
+            const underlyingBalance = document.createElement('p');
+            underlyingBalance.className = 'asset__value';
+            underlyingBalance.innerHTML = `<strong>Balance Of Underlying:</strong> ${tokenData.BalanceOfUnderlying}`;
+            assetSupplyBorrowValues.appendChild(underlyingBalance);
+           
+          
+        }
+      });
+    });
+  }
+}
+
+const form = new Form();
+form.onSubmit();
---
+++ server.js
@@ -0,0 +1,187 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Web3 = require('web3');
+const lodash = require('lodash');
+const app = express();
+const router = express.Router();
+const url = require('url');
+const path = require('path');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const http = require('http');
+const fs = require('fs')
+var cors = require('cors');
+
+// Loading config files
+const cAddressesConfig = require('./config/cAddresses_Config.json');
+const appConfig = require('./config/app_Config.json');
+const defaultConfig = appConfig.development;
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = appConfig[environment];
+const finalConfig = lodash.merge(defaultConfig, environmentConfig);
+
+// as a best practice all global variables should be referenced via global syntax and their names should always begin with g
+global.gConfig = finalConfig;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Initialising json object
+var jsonData = {} // empty Object
+var key = 'Balances';
+jsonData[key] = []; // empty Array, which you can push() values into
+
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+//Initialising private key variable
+const walletPrivateKey = global.gConfig.walletPrivateKey; 
+//Initialising web3 object 
+const web3 = new Web3('https://mainnet.infura.io/v3/4bdfcdc7dc064fbe81ed335e2706a32b');
+
+web3.eth.accounts.wallet.add(walletPrivateKey);
+var myWalletAddress = "";
+const ethDecimals = 18;
+
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/',function(req,res) {
+res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.route('/wallet-balance/ceth/').get((req, res) => {
+    myWalletAddress = req.query.address;
+    let isValidEthAddress = Web3.utils.isAddress(myWalletAddress);
+	console.log(isValidEthAddress);
+	if(isValidEthAddress){
+	jsonData[key] = [];
+	start(function() {	
+	res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(jsonData, 0, 2));
+	
+    }).catch((err) => {
+        console.error(err);
+    });;
+	}
+	else{
+		
+		jsonData[key] = [];
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify(jsonData, 0, 2));
+	}
+});
+
+
+
+async function start(callback) {
+    const ethMantissa = 1e18;
+    const blocksPerDay = 6570; // 13.15 seconds per block
+    const daysPerYear = 365;
+
+    const cTokenAbi = global.gConfig.cTokenAbi;
+	const erc20Abi = global.gConfig.erc20Abi;
+
+    let main = []
+    let openJSON = fs.readFileSync('./config/cAddresses_Config.json', 'utf-8');
+    let parseJSON = JSON.parse(openJSON);
+
+    for (let i = 0; i < parseJSON.addresses.length; i++) {
+        const cTokenAddress = parseJSON.addresses[i]["address"];
+		const tokenAddress = parseJSON.addresses[i]["ercaddress"];
+
+        const cToken = new web3.eth.Contract(cTokenAbi, cTokenAddress);
+		const underlyingToken = new web3.eth.Contract(erc20Abi, tokenAddress);
+
+
+        const supplyRatePerBlock = await cToken.methods.supplyRatePerBlock().call();
+        const borrowRatePerBlock = await cToken.methods.borrowRatePerBlock().call();
+        var supplyApy = (((Math.pow((supplyRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear))) - 1) * 100;
+        var borrowApy = (((Math.pow((borrowRatePerBlock / ethMantissa * blocksPerDay) + 1, daysPerYear))) - 1) * 100;
+        supplyApy = roundToTwo(supplyApy);
+        borrowApy = roundToTwo(borrowApy);
+        var balance = await cToken.methods.balanceOf(myWalletAddress).call();
+
+        var exchangeRateCurrent = await cToken.methods.exchangeRateCurrent().call();
+        exchangeRateCurrent = exchangeRateCurrent / Math.pow(10, 18 + ethDecimals - 8);
+
+        const balanceOfUnderlying = web3.utils.toBN(await cToken.methods.balanceOfUnderlying(myWalletAddress).call()) / Math.pow(10, ethDecimals);
+	
+		const cTokenDecimals = 8; // all cTokens have 8 decimal places
+		const underlyingDecimals = await underlyingToken.methods.decimals().call();
+		const mantissa = 18 + parseInt(underlyingDecimals) - cTokenDecimals;
+		const oneCTokenInUnderlying = exchangeRateCurrent / Math.pow(10, mantissa);
+		 
+        console.log(`Name ${parseJSON.addresses[i]["name"]}`);
+        console.log(`Supply1 APY for ETH ${supplyApy} %`);
+        console.log(`Borrow1 APY for ETH ${borrowApy} %`);
+        console.log(`Balance ${balance}`);
+        console.log(`CToken UnderlyingBalance ${balanceOfUnderlying}`);
+		console.log(`One cToken Underlying ${oneCTokenInUnderlying}`);
+        console.log(' --------------------------');
+        console.log('');
+
+
+		if(balance!=0){
+        var data = {
+            cTokenName: parseJSON.addresses[i]["name"],
+            cTokenAddress: cTokenAddress,
+            WalletAddress: myWalletAddress,
+            SupplyAPY: supplyApy,
+            BorrowAPY: borrowApy,
+            Balance: balance,
+			RedeemRate: oneCTokenInUnderlying,
+			BalanceOfUnderlying:balanceOfUnderlying
+			
+        };
+
+        jsonData[key].push(data);
+		}
+
+    }
+    callback();
+
+
+
+
+}
+
+function roundToTwo(num) {
+    return +(Math.round(num + "e+2") + "e-2");
+}
+
+
+app.use(cors({origin: 'null'}));
+
+
+
+// setting the application to listen on port configured in the config file
+const server = app.listen(process.env.PORT || global.gConfig.node_port, function() {
+    const host = server.address().address
+    const port = server.address().port
+    console.log("Fetch Tweet Details app listening at %s", port)
+
+})
